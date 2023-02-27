360a2f2e769e92195ec8ea73318bed77bb1344f8
Jovana Filipovic <jovana.filipovic@endava.com>
2023-02-27 22:54:04 +0100

    Edit and update existing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,38 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        # render plain: params[:article] --- KAKO BI PRILIKOM KLIKA DOBILI INFORMACIJU SA PODACIMA U JSON FORMATU
+        #MEDJUTIM, MI NE ZELIMO SAMO DA PRIKAZUJEMO PODATKE, VEC I DA IH CUVAMO U BAZI
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect --- AKO ZELIMO DA PRIKAZEMO NA EKRANU
+        
+        if @article.save #CUVAMO U BAZI
+            flash[:notice] = "Article saved successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+       # redirect_to article_path(@article) #REDIREKTOVACE NA "SHOW" STRANU KAKO BI PRIKAZAO DETALJE O NOVOKREIRANOM ARTIKLU
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        
+        if @article.update(params.require(:article).permit(:title, :description)) #OVO CE IMATI ISTI EFEKAT KAO "SAVE" GORE, SAMO STO OVDE MENJAMO
+            flash[:notice] = "Article was updated successfully." #OVIM SE ISPISUJE PORUKA
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
 end
